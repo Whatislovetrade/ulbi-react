b49cbe41654a0f0a7f5ce0f0a7178618ef3f996c
Vlad <vladmaslow1@gmail.com>
2025-05-20 23:07:20 +0500

    настроил dev-server от вебпака, моя ошибка была что не подстроил версию ноды, в общем внимательнее нужно смотреть с какой работаю

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,11 +1,12 @@
-import HtmlWebpackPlugin from "html-webpack-plugin"
-import webpack from "webpack"
-import { BuildOptions } from "./types/config"
+import HTMLWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
+import {BuildOptions} from "./types/config";
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+
     return [
-        new HtmlWebpackPlugin({
-            template: paths.html
+        new HTMLWebpackPlugin({
+            template: paths.html,
         }),
         new webpack.ProgressPlugin(),
     ]
